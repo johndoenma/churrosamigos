a819c3688f3fc9aa7b3ee0db9ac44b10ecf6b6ef
kccnma <gargiulo@hawaii.edu>
2024-04-29 16:23:33 -1000

    initital test using davids parallax js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
+// PARALLAX
+function parallax({ el, ease = "linear" }) {
+  let ticking = false;
+  
 
-    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
-    const menubutton = document.querySelector('.menu-button');
-    const menunav = document.querySelector('.toggle-nav');
-    menubutton.addEventListener('click', function () {
-        if (menunav.getAttribute('data-navstate') === 'open') {
-            // if true do this
-            menunav.setAttribute('data-navstate', 'closed');
-        } else {
-            // else (if false) do this:
-            menunav.setAttribute('data-navstate', 'open');
-        };
-    });
+  function runParallax() {
+    const pageTop = window.pageYOffset;
+    const pageMid = pageTop + window.innerHeight / 2;
 
-    //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
-    var stickynavlinks = document.querySelectorAll(".sticky nav a");
-    for (var i = 0; i < stickynavlinks.length; i++) {
-        stickynavlinks[i].onclick = function () {
-            menunav.setAttribute('data-navstate', 'closed');
-        }
-    };
+    // Run this if it already has a data attribute set
+    const topSection = el.offsetTop;
+    const midSection = topSection + el.offsetHeight / 2;
 
-});
+    const viewDistanceLeft = pageMid - midSection;
+    const parallaxSpeed = parseFloat(el.getAttribute("data-parallax-speed"));
+
+    // Sets the parallax direction
+    let direction = "";
+
+    if (el.hasAttribute("data-parallax-direction")) {
+      // Direction to value of the attribute
+      direction = el.getAttribute("data-parallax-direction");
+    } else {
+      // Set default direction to vertical
+      direction = "vertical";
+    }
+
+    // Handle transform based on direction
+    if (direction.toLowerCase() === "vertical") {
+      el.style.transform = `translate3d(0, ${
+        (viewDistanceLeft * parallaxSpeed) / 3
+      }px, 0)`;
+    } else if (direction.toLowerCase() === "horizontal") {
+      el.style.transform = `translate3d( ${
+        (viewDistanceLeft * parallaxSpeed) / 3
+      }px, 0, 0)`;
+    }
+
+    el.style.transition = `transform ${ease}`;
+  }
+
+  window.addEventListener("scroll", () => {
+    
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        runParallax();
+        ticking = false;
+      });
+    }
+    ticking = true;
+  });
+}
+
+// Parallax
+const parallaxElements = document.querySelectorAll(".parallax-layer");
+parallaxElements.forEach((el) => parallax({ el }));
